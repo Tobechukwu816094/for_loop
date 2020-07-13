3e35162e49f145720bd46a6f1e1914da917f1dda
Tobechukwu816094 <ugbaajat21@gmail.com>
2020-07-13 10:46:53 +0100

    day six commit

---
+++ no3/for _loop.py
@@ -0,0 +1,25 @@
+"""
+use a for loop to print a box like the one below.Allow the user to specify 
+how wide and how high the box should be.
+
+***************
+***************
+***************
+***************
+***************
+***************
+
+"""
+wideth =int(input("please enter wideth of box:"))
+height = int(input("please enter height of box:"))
+for i in range( height ):
+    print('*'*wideth ) 
+
+
+
+
+
+
+
+
+
